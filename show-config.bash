51e35b4eacf0127a9fed08868bc75e52347312a4
Carl Simon Adorf <csadorf@umich.edu>
2018-06-14 14:03:14 -0400

    Mock and check the pypirc config file.

--- show-config.bash
+++ show-config.bash
@@ -1,1 +1,12 @@
-echo usernamelength: ${#PYPI_USERNAME}
+cat <<'EOF' >> ~/.pypirc
+[distutils]
+index-servers=
+    pypi
+    testpypi
+
+[pypi]
+repository: https://pypi.python.org/pypi
+username: ${#PYPI_USERNAME}
+EOF
+
+cat ~/.pypirc
